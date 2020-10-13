4792080143a20c70be8f3c89f8d25ce224d259cd
Lea Dalibard <leadalibard@gmail.com>
2020-10-13 09:30:26 +0200

    Adding button Hit, Stand, Surrender

--- index.php
+++ index.php
@@ -23,5 +23,52 @@ $dealer=$blackjack->getDealer();
 
 var_dump($player->hit());
 
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Blackjack</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+
+<section class="container">
+
+    <form method="post" action="index.php">
+
+        <button type="submit" name="hit" class="btn btn-primary">Hit</button>
+        <button type="submit" name="stand" class="btn btn-primary">Stand</button>
+        <button type="submit" name="surrender" class="btn btn-primary">Surrender</button>
+
+
+    </form>
+
+</section>
+
+
+
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+</body>
+</html>
+
 
 
